e549014de77640c6e3fcb7cf0f8b1ab5481ee493
Javier Parra <javierparraoyola619@hotmail.com>
2018-01-11 00:44:30 +0100

    fix: arreglado error sintaxis

--- Trabajo/functions.php
+++ Trabajo/functions.php
@@ -30,15 +30,11 @@ function user_register($name, $surname, $phone, $email, $country, $code){
 
 function creditcard_check($ccn, $cvv, $ccyear, $ccmonth  ){
 	$check = TRUE;
-	$pattern_1 ='/^((4[0-9]{12})|(4[0-9]{15})|(5[1-5][0-9]{14})|(3[47][0-9]{13})|(6011[0-9]{12}))$/';
-    $pattern_2 = '/^((30[0-5][0-9]{11})|(3[68][0-9]{12})|(3[0-9]{15})|(2123[0-9]{12})|(1800[0-9]{12}))$/';
-	$pattern_3
+	$pattern_1 = "^[0-9]{16}$";
 	
 	//Comprobacion de que el numero de la tarjeta cumpla el pattern
 	
 	if(preg_match($pattern_1, $ccn)  ){
-		$check =TRUE;
-	}else if(preg_match($pattern_2, $ccn)){
 		$check =TRUE;
 	}else{
 		$check = FALSE;
@@ -46,8 +42,13 @@ function creditcard_check($ccn, $cvv, $ccyear, $ccmonth  ){
 	
 	//Comprobacion de que el CVV sean tres digitos
 	
-	if(strlen($cvv) != 3 || $ccdate == "" ){
+	if(strlen($cvv) != 3 || $ccdate == ""){
 		$check = FALSE;
+	}
+	else{
+		if(!preg_match("^[0-9]{3}$", $cvv)){
+			$check = FALSE;
+		}
 	}
 	
 	// Comprobacion de fechas
@@ -72,7 +73,7 @@ function creditcard_check($ccn, $cvv, $ccyear, $ccmonth  ){
 function code_promotional($code , $ccn, $cvv, $ccyear, $ccmonth ){
 	$check = FALSE;
 	
-	if(preg_match('(^$)|(^[aA-zZ]{6}[0-9]{3}$)', $code){
+	if(preg_match('(^$)|(^[aA-zZ]{6}[0-9]{3}$)', $code)){
 			if($ccn == "" || $cvv == "" || $ccyear == "" || $ccmonth == ""){
 				$check = TRUE;
 			}
