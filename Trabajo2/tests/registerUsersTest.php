0a466288f905f58f80a4d4df730a0466b717e4fe
julio <julioomar4c@gmail.com>
2018-02-02 13:09:51 +0100

    prueba

---
+++ Trabajo2/tests/registerUsersTest.php
@@ -0,0 +1,76 @@
+<?php
+	 include_once("functions.php");
+
+class Tests extends PHPUnit_Framework_TestCase
+{
+	
+    public function test()
+    { 
+$register = 'checkRegister';
+$this->$register();
+
+$emptyName = 'checkEmptyName';
+$this->$emptyName();
+
+$emptySurname = 'checkEmptySurname';
+$this->$emptySurname();
+
+$emptyEmail = 'checkEmptyEmail';
+$this->$emptyEmail();
+
+$email = 'checkEmail';
+$this->$email();
+
+$code = 'checkCode';
+$this->$code();
+
+}
+	function checkRegister(){
+		$bool = user_check("name", "surname", "", "email@email.com", "España", "");
+		
+		if($bool == TRUE){
+			echo "Successfully registered.";
+		}
+	}
+	
+	function checkEmptyName(){
+		$bool = user_check("", "surname", "", "email@email.com", "España", "");
+		
+		if($bool == FALSE){
+			echo "There is not a name.";
+		}
+	}
+	
+	function checkEmptySurname(){
+		$bool = user_check("name", "", "", "email@email.com", "España", "");
+		
+		if($bool == FALSE){
+			echo "There is not a surname.";
+		}
+	}
+	
+	function checkEmptyEmail(){
+		$bool = user_check("name", "surname", "", "", "España", "");
+		
+		if($bool == FALSE){
+			echo "There is not an email.";
+		}
+	}
+	
+	function checkEmail(){
+		$bool = user_check("name", "surname", "", "email", "España", "");
+		
+		if($bool == FALSE){
+			echo "Incorrect email.";
+		}
+	}
+	
+	function checkCode(){
+		$bool = user_check("name", "surname", "", "email@email.com", "España", "ABCDEF");
+		
+		if($bool == FALSE){
+			echo "Incorrect code.";
+		}
+	}
+}
+?>
